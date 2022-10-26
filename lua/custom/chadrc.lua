ef57359d5a12a039446ae315c07127cd4a61156f
jtzero ? <jtzero511@gmail.com>
2022-10-26 09:45:35 -0400

    upgrades to new stable snapshot

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -28,66 +28,23 @@ M.ui = {
    },
 }
 
-M.options = {
-    user = function()
-      local enable_providers = {
-        "python3_provider",
-        "node_provider",
-        "ruby_provider",
-        -- and so on
-      }
-      for _, plugin in pairs(enable_providers) do
-        vim.g["loaded_" .. plugin] = nil
-      --  vim.cmd("runtime " .. plugin)
-      end
-    end,
+local enable_providers = {
+  "python3_provider",
+  "node_provider",
+  "ruby_provider",
+  -- and so on
 }
-
+for _, plugin in pairs(enable_providers) do
+  vim.g["loaded_" .. plugin] = nil
+  vim.cmd("runtime " .. plugin)
+end
 M.plugins = {
-  options = {
-    --lspconfig = {
-    --  setup_lspconfig = "",
-    --}
-  },
-  remove = {
-      "windwp/nvim-autopairs", -- auto createes closing paren bracket quote etc
-      "lukas-reineke/indent-blankline.nvim", -- highlights blocks and provides vertical lines on indent
-  },
-  -- enable and disable plugins (false for disable)
-  status = {
-    bufferline = true, -- buffer shown as tabs
-    cheatsheet = true, -- fuzzy search your commands/keymappings
-    gitsigns = true, -- gitsigns in statusline
-    lspsignature = true, -- lsp enhancements
-    neoformat = true, -- universal formatter
-    neoscroll = true, -- smooth scroll
-    telescope_media = false, -- see media files in telescope picker
-    truezen = false, -- no distraction mode for nvim
-    vim_fugitive = false, -- git in nvim
-  },
-  install = {
-      ['ntpeters/vim-better-whitespace'] = {},
-      ["folke/which-key.nvim"] = { disable = false },
-  },
-  user = {
-    ["NvChad/extensions"] = {
-        commit = "785eaa25a9bbdf47a6808dc5b6da1747abe10b2b"
-      },
-  },
-  override = {
-    ["nvim-treesitter/nvim-treesitter"] = {
-      auto_install = true,
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-        disable = { "python" },
-      },
-    },
-    ["NvChad/nvim-colorizer.lua"] = {
-      filetypes = {
-        '*',
-        '!python',
-      },
-    },
+  ["windwp/nvim-autopairs"] = false, -- auto createes closing paren bracket quote etc
+  ["lukas-reineke/indent-blankline.nvim"] = false, -- highlights blocks and provides vertical lines on indent
+  ['ntpeters/vim-better-whitespace'] = {},
+  ["folke/which-key.nvim"] = { disable = false },
+  ["nvim-treesitter/nvim-treesitter"] = {
   }
 }
+
+return M
